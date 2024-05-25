acc48c3d23aec311fe7e9e13d5469e8857cbd3b7
Favor Vincent <vfavorm77@gmail.com>
2024-05-25 21:47:06 +0300

    anagram lb complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,20 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+      self.word = word  
+
+    def match(self, possible_anagrams):
+        anagrams = []
+        for  a in  possible_anagrams:
+            letters = [letter for letter in a]
+            init_letters = (l for l in self.word)
+            if sorted(letters) == sorted(init_letters):
+                  anagrams.append(a)
+
+        if len(anagrams) > 0 :
+            print(anagrams)
+            return anagrams
+        else : return []
+
+listen = Anagram("listen")
+listen.match(['enlists', 'google', 'inlets', 'banana'])
